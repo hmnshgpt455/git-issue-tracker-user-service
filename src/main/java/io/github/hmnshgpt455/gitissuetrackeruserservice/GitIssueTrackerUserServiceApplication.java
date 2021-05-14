7176a12cfe88ccf1bce572ce6fa9bcb6019e20f1
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-05-14 17:59:05 +0530

    Adding initial project for User service and also adding discovery configuration. Closes #3, Closes #4.

---
+++ src/main/java/io/github/hmnshgpt455/gitissuetrackeruserservice/GitIssueTrackerUserServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.gitissuetrackeruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIssueTrackerUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIssueTrackerUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/hmnshgpt455/gitissuetrackeruserservice/GitIssueTrackerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.gitissuetrackeruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIssueTrackerUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
