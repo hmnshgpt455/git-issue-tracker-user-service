7d8510a01230dec6af111dccd822baf39d5c8db5
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-05-17 09:36:55 +0530

    Hiding availability methods in User repository from Spring Data Rest. Closes #10.

--- src/main/java/io/github/hmnshgpt455/gitissuetrackeruserservice/repositories/UserRepository.java
+++ src/main/java/io/github/hmnshgpt455/gitissuetrackeruserservice/repositories/UserRepository.java
@@ -3,12 +3,16 @@ package io.github.hmnshgpt455.gitissuetrackeruserservice.repositories;
 import io.github.hmnshgpt455.gitissuetrackeruserservice.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    @RestResource(exported = false)
     boolean existsByUsername(String username);
+
+    @RestResource(exported = false)
     boolean existsByEmail(String email);
 }
